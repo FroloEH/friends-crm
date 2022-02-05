d2a396f42b8476cf8f21672cb2cd9d522420af43
FroloEH <richard.frolkovic@event-horizon.sk>
2022-02-05 20:02:56 +0100

    Added authentication service and page.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,40 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+import TabsPage from "../views/TabsPage.vue";
+import AuthenticationPage from "../views/AuthenticationPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/tabs/contacts'
+    path: "/",
+    redirect: "/tabs/contacts",
   },
   {
-    path: '/tabs/',
+    path: "/tabs/",
     component: TabsPage,
     children: [
       {
-        path: '',
-        redirect: '/tabs/contacts'
+        path: "",
+        redirect: "/tabs/contacts",
       },
       {
-        path: 'contacts',
-        component: () => import('@/views/ContactListTab.vue')
+        path: "contacts",
+        component: () => import("@/views/ContactListTab.vue"),
       },
       {
-        path: 'add-contact',
-        component: () => import('@/views/AddContactTab.vue')
+        path: "add-contact",
+        component: () => import("@/views/AddContactTab.vue"),
       },
-    ]
+    ],
+  },
+  {
+    path: "/auth",
+    component: AuthenticationPage,
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
